27b92c681ef208e74b7635e1d019ea8812051876
Abubakirov Vladimir <abubakirov04@mail.ru>
2022-05-10 00:41:07 +0300

    Small changes in template

---
+++ src/main/java/com/example/nomoneytrade/controller/AppController.java
@@ -0,0 +1,24 @@
+
+
+package com.example.nomoneytrade.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String index() {
+        return "<h1>Try to register. Ne poluchitsa ha-ha</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "<h1>Hey, admin</h1>";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "<h1>Hey, user!</h1>";
+    }
+}
